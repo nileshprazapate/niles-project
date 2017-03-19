e0d27767e6c7348eedc69e7271dba8d3fb1317c5
nileshprazapate <rockstar123.np@gmail.com>
2017-03-20 00:02:15 +0545

    example 1

--- name.py
+++ name.py
@@ -1,5 +1,5 @@
-first_name=niles
-last_name=prazapate
-home_town=chapacho
+first_name="niles"
+last_name="prazapate"
+home_town="chapacho"
 print("hi! i am", first_name ,last_name)
 print("I'm from",home_town)
