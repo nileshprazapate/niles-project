9c7596a0df0acabdf143c94a0518c9a6bda74415
nileshprazapate <rockstar123.np@gmail.com>
2017-03-24 12:47:12 +0545

    condition ko execirse

--- exercise1.py
+++ exercise1.py
@@ -1,13 +1,15 @@
 point1=float(input('entered co ordinate of x : '))
 point2=float(input('entered co ordinate of y : '))
-if point1>0 and point2>0:
+
+if point1==0 and point2==0:
+    print('lies on origin')
+elif point1>0 and point2>0:
     print('lies on 1st')
-elif point1>0 and point2<0:
-    print('lies on 4nd')
+elif point1<0 and point2>0:
+    print('lies on 2nd')
 elif point1<0 and point2<0:
     print('lies on 3rd')
-elif point1<0and point2>0:
-    print('lies on 2nd')
-else:
-    print('invaild')
+elif point1>0 and point2<0:
+    print('lies on 4nd')
+
  
